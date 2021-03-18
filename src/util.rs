752ad0db9941afb302deee2691945abf43df20fd
Markus Dam <markus.dam123@gmail.com>
2021-03-18 19:51:08 +0100

    Swap order of arguments on clamp function

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 use std::cmp;
 
-pub fn clamp<T>(value: T, max_value: T, min_value: T) -> T where T: Ord {
+pub fn clamp<T>(value: T, min_value: T, max_value: T) -> T where T: Ord {
     return cmp::min(max_value, cmp::max(min_value, value));
 }
